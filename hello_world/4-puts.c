517a1c33a680576a7a1892546bcbd4bed851a549
EljanBadalov <elcanbadalov06@gmail.com>
2025-11-26 17:15:00 +0000

    Add solution for task 4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/*
-* main - Entry point of the program
-*
-* Return: Always 0 (Success)
-*/
+/**
+ * main - This uses program prints exactly the string
+ * "Programming is like building a multilingual puzzle
+ * followed by a new line.
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
