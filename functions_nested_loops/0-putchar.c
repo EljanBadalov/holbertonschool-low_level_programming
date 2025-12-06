ae4492affeefcba502cbf1fcc92d8631aa9f8ec5
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2025-12-06 23:48:51 +0400

    Change printed output to '_putchar'
    Update the main function to print '_putchar' instead of 'Holberton'.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - Prints "Holberton" followed by a new line.
+ * main - prints _putchar followed by a new line
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
 	_putchar('_');
-	_putchar('P');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
