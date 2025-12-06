a16c784514dd2015bc3cd56614a7512c55129ab7
Eljan Badalov <123768216+EljanBadalov@users.noreply.github.com>
2025-12-07 00:01:03 +0400

    Refactor print_last_digit function and comments
    Updated function documentation and improved last digit calculation.

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,23 +1,21 @@
-#include <unistd.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_last_digit - This function prints the last digit of a number
- * @n: The integer the function is receiving
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to check
  *
- * Return: the value of the last digit of n
+ * Return: the value of the last digit
  */
 int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = (n % 10);
+	last_digit = n % 10;
 
 	if (last_digit < 0)
-	{
-		last_digit = last_digit * -1;
-	}
+		last_digit = -last_digit;
 
 	_putchar(last_digit + '0');
+
 	return (last_digit);
 }
