52de1e500a56c9df7c5b4d51889da0439301e828
EljanBadalov <elcanbadalov06@gmail.com>
2025-12-13 20:07:50 +0000

    I change some task

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,33 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
- *          null byte, to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
  *
- * Return: A pointer to the destination string @dest.
+ * Return: pointer to dest
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0;
+	int j = 0;
 
-	while (dest[index++])
-		dest_len++;
+	/* dest-in sonunu tap */
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	/* src-ni dest-in sonuna kopyala */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* yeni string-i bitir */
+	dest[i] = '\0';
 
 	return (dest);
 }
